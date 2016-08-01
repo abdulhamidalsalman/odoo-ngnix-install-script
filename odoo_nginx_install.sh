a056eab8fc0618f02bcb0b1c8dcb62d7ceed2791
Abdulhamid Ibrahim Alsalman <abdulhamid.alsalman@gmail.com>
2016-08-01 23:44:17 +0300

    Update odoo_nginx_install.sh

--- odoo_nginx_install.sh
+++ odoo_nginx_install.sh
@@ -18,26 +18,34 @@
 #--------------------------------------------------
 # Fixed Parameters
 #--------------------------------------------------
-OE_USER="odoo"
-OE_HOME="/$OE_USER"
-OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
+ODOO_USER="odoo"
+ODOO_HOME="/$ODOO_USER"
+ODOO_HOME_EXT="/$ODOO_USER/${ODOO_USER}-server"
 
 
 #Choose the port on which your Odoo should run (xmlrpc-port)
-OE_PORT="8069"
+ODOO_PORT="8069"
 
 #Enter version for checkout "9.0" for version 9.0,"8.0" for version 8.0, "7.0 (version 7), "master" for trunk
-OE_VERSION="9.0"
+ODOO_VERSION="9.0"
 
 #set the superadmin password
-OE_SUPERADMIN="admin"
-OE_CONFIG="${OE_USER}-server"
+ODOO_SUPERADMIN="admin"
+ODOO_CONFIG="${ODOO_USER}-server"
 
 #nginx reverse proxy parameters
-NGINX_URL="odoo.mycompany.com"
-NGINX_URL_PREFIX="https://"
+DOMAIN_NAME="odoo.mycompany.com" #change with your domain
+SRVR_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ODOO_IP="$SRVR_IP"
+ODOO_SRVR="$DOMAIN_NAME"
+DOMAIN_NAME_PREFIX="https://"
 NGINX_PORT="443"
-NGINX_CONFIG="$NGINX_URL" #file name for config
+NGINX_CONFIG="$DOMAIN_NAME" #file name for config
+
+#SSL Configuration
+SSL_EMAIL="admin@example.com" #email for let's encrypt info
+SSL_CERT=/root/.acme.sh/$DOMAIN_NAME/fullchain.cer              #ssl_certificate
+SSL_CERTK=/root/.acme.sh/$DOMAIN_NAME/${DOMAIN_NAME}.key        #ssl_certificate_key
 
 #--------------------------------------------------
 # WKHTMLTOPDF
@@ -72,7 +80,7 @@ echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
 sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.3/main/postgresql.conf
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
+sudo su - postgres -c "createuser -s $ODOO_USER" 2> /dev/null || true
 
 sudo service postgresql restart
 
@@ -81,20 +89,20 @@ sudo service postgresql restart
 #--------------------------------------------------
 
 echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$ODOO_HOME --gecos 'ODOO' --group $ODOO_USER
 
 #The user should also be added to the sudo'ers group.
-#sudo adduser $OE_USER sudo
+#sudo adduser $ODOO_USER sudo
 
 echo -e "\n---- Create Log directory ----"
-sudo mkdir /var/log/$OE_USER
-sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
+sudo mkdir /var/log/$ODOO_USER
+sudo chown $ODOO_USER:$ODOO_USER /var/log/$ODOO_USER
 
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n---- Install tool packages ----"
-sudo apt-get install wget subversion git bzr bzrtools python-pip python-imaging python-setuptools python-dev libxslt-dev libxml2-dev libldap2-dev libsasl2-dev gdebi-core -y
+sudo apt-get install wget unzip subversion git bzr bzrtools python-pip python-imaging python-setuptools python-dev libxslt-dev libxml2-dev libldap2-dev libsasl2-dev gdebi-core -y
 	
 echo -e "\n---- Install python packages ----"
 sudo apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil -y
@@ -106,6 +114,29 @@ echo -e "\n--- Install other required packages"
 sudo apt-get install node-clean-css -y
 sudo apt-get install node-less -y
 sudo apt-get install python-gevent -y
+sudo apt-get install openssl bc curl
+
+#--------------------------------------------------
+# Let's encrypt install and configuration for SSL
+#--------------------------------------------------
+echo "*********************************"
+echo "*                               *"
+echo "*    Getting Let's encrypt      *"
+echo "*                               *"
+echo "*********************************"
+echo -e "\n---- Install acme.sh for Let's encrypt  ----"
+git clone https://github.com/Neilpang/acme.sh.git
+cd acme.sh
+./acme.sh --install  \
+--accountemail  $SSL_EMAIL \
+cd ~
+
+cho -e "\n---- Install SSL Certificates for your domains  ----"
+~/.acme.sh/acme.sh --issue -d $DOMAIN_NAME -d www.${DOMAIN_NAME} -d $ODOO_SRVR -w /usr/share/nginx/html
+
+echo -e "\n---- Generate Strong Diffie-Hellman Group.  ----"
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
 
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
@@ -132,52 +163,52 @@ fi
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $ODOO_VERSION https://www.github.com/odoo/odoo $ODOO_HOME_EXT/
 
 echo -e "\n---- Create custom module directory ----"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
+sudo su $ODOO_USER -c "mkdir $ODOO_HOME/custom"
+sudo su $ODOO_USER -c "mkdir $ODOO_HOME/custom/addons"
 
 echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
+sudo chown -R $ODOO_USER:$ODOO_USER $ODOO_HOME/*
 
 #--------------------------------------------------
 # Configure ODOO
 #--------------------------------------------------
 echo -e "* Create server config file"
-sudo cp $OE_HOME_EXT/debian/openerp-server.conf /etc/${OE_CONFIG}.conf
-sudo chown $OE_USER:$OE_USER /etc/${OE_CONFIG}.conf
-sudo chmod 640 /etc/${OE_CONFIG}.conf
+sudo cp $ODOO_HOME_EXT/debian/openerp-server.conf /etc/${ODOO_CONFIG}.conf
+sudo chown $ODOO_USER:$ODOO_USER /etc/${ODOO_CONFIG}.conf
+sudo chmod 640 /etc/${ODOO_CONFIG}.conf
 
 echo -e "* Change server config file"
 echo -e "** Remove unwanted lines"
-sudo sed -i "/db_user/d" /etc/$OE_CONFIG.conf
-sudo sed -i "/admin_passwd/d" /etc/$OE_CONFIG.conf
-sudo sed -i "/addons_path/d" /etc/$OE_CONFIG.conf
-sudo sed -i "/xmlrpc_port/d" /etc/$OE_CONFIG.conf
+sudo sed -i "/db_user/d" /etc/$ODOO_CONFIG.conf
+sudo sed -i "/admin_passwd/d" /etc/$ODOO_CONFIG.conf
+sudo sed -i "/addons_path/d" /etc/$ODOO_CONFIG.conf
+sudo sed -i "/xmlrpc_port/d" /etc/$ODOO_CONFIG.conf
 
 echo -e "** Add correct lines"
-sudo su root -c "echo 'db_user = $OE_USER' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "echo 'admin_passwd = $OE_SUPERADMIN' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "echo 'logfile = /var/log/$OE_USER/$OE_CONFIG$1.log' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "echo 'addons_path=$OE_HOME_EXT/addons,$OE_HOME/custom/addons' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "echo 'xmlrpc_port = $OE_PORT' >> /etc/$OE_CONFIG.conf"
+sudo su root -c "echo 'db_user = $ODOO_USER' >> /etc/$ODOO_CONFIG.conf"
+sudo su root -c "echo 'admin_passwd = $ODOO_SUPERADMIN' >> /etc/$ODOO_CONFIG.conf"
+sudo su root -c "echo 'logfile = /var/log/$ODOO_USER/$ODOO_CONFIG$1.log' >> /etc/$ODOO_CONFIG.conf"
+sudo su root -c "echo 'addons_path=$ODOO_HOME_EXT/addons,$ODOO_HOME/custom/addons' >> /etc/$ODOO_CONFIG.conf"
+sudo su root -c "echo 'xmlrpc_port = $ODOO_PORT' >> /etc/$ODOO_CONFIG.conf"
 
 
 echo -e "* Create startup file"
-sudo su root -c "echo '#!/bin/sh' >> $OE_HOME_EXT/start.sh"
-sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/openerp-server --config=/etc/${OE_CONFIG}.conf' >> $OE_HOME_EXT/start.sh"
-sudo chmod 755 $OE_HOME_EXT/start.sh
+sudo su root -c "echo '#!/bin/sh' >> $ODOO_HOME_EXT/start.sh"
+sudo su root -c "echo 'sudo -u $ODOO_USER $ODOO_HOME_EXT/openerp-server --config=/etc/${OE_CONFIG}.conf' >> $ODOO_HOME_EXT/start.sh"
+sudo chmod 755 $ODOO_HOME_EXT/start.sh
 
 #--------------------------------------------------
 # Adding ODOO as a deamon (initscript)
 #--------------------------------------------------
 
 echo -e "* Create init file"
-cat <<EOF > ~/$OE_CONFIG
+cat <<EOF > ~/$ODOO_CONFIG
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides: $OE_CONFIG
+# Provides: $ODOO_CONFIG
 # Required-Start: \$remote_fs \$syslog
 # Required-Stop: \$remote_fs \$syslog
 # Should-Start: \$network
@@ -188,11 +219,11 @@ cat <<EOF > ~/$OE_CONFIG
 # Description: ODOO Business Applications
 ### END INIT INFO
 PATH=/bin:/sbin:/usr/bin
-DAEMON=$OE_HOME_EXT/openerp-server
-NAME=$OE_CONFIG
-DESC=$OE_CONFIG
+DAEMON=$ODOO_HOME_EXT/openerp-server
+NAME=$ODOO_CONFIG
+DESC=$ODOO_CONFIG
 # Specify the user name (Default: odoo).
-USER=$OE_USER
+USER=$ODOO_USER
 # Specify an alternate config file (Default: /etc/openerp-server.conf).
 CONFIGFILE="/etc/${OE_CONFIG}.conf"
 # pidfile
@@ -241,15 +272,15 @@ exit 0
 EOF
 
 echo -e "* Security Init File"
-sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-sudo chmod 755 /etc/init.d/$OE_CONFIG
-sudo chown root: /etc/init.d/$OE_CONFIG
+sudo mv ~/$ODOO_CONFIG /etc/init.d/$ODOO_CONFIG
+sudo chmod 755 /etc/init.d/$ODOO_CONFIG
+sudo chown root: /etc/init.d/$ODOO_CONFIG
 
 echo -e "* Start ODOO on Startup"
-sudo update-rc.d $OE_CONFIG defaults
+sudo update-rc.d $ODOO_CONFIG defaults
 
 echo -e "* Starting Odoo Service"
-sudo su root -c "/etc/init.d/$OE_CONFIG start"
+sudo su root -c "/etc/init.d/$ODOO_CONFIG start"
 
 #--------------------------------------------------
 # Installing NGINX reverse proxy
@@ -263,35 +294,43 @@ echo -e "\n- NGINX config file location = $NGINX_CONFIG_AVAILABLE"
 echo -e "* Create init file"
 cat <<EOF > ~/$NGINX_CONFIG
 server {
-	listen 80;
-	server_name $NGINX_URL;
-	add_header Strict-Transport-Security max-age=2592000;
-	rewrite ^/.*$ https://escape"$host$request_uri"? permanent;
+  listen 80;
+  server_name $DOMAIN_NAME;
+  add_header Strict-Transport-Security max-age=2592000;
+  rewrite ^/.*$ https://escape"$host$request_uri"? permanent;
 } 
      
 server {
   listen 443;
-  server_name $NGINX_URL;
+  server_name $DOMAIN_NAME;
   proxy_set_header Host escape"$host";
   proxy_buffering off;
+  
+  # add ssl specific settings
+  # keepalive_timeout    240;
+  access_log  /var/log/nginx/oddo.access.log;
+  error_log   /var/log/nginx/oddo.error.log;    
+  ssl                          on;
+  ssl_certificate              /etc/nginx/ssl/server.crt; 
+  ssl_certificate_key          /etc/nginx/ssl/server.key;
+  ssl_session_timeout          10h; 
+  ssl_protocols                SSLv3 TLSv1;
+  ssl_ciphers                  HIGH:!ADH:!MD5; 
+  ssl_prefer_server_ciphers    on;
+  keepalive_timeout   240; 
+  
+      gzip on;
+    gzip_disable "msie6";
+    gzip_vary on;
+    gzip_proxied any;
+    gzip_comp_level 6;
+    gzip_buffers 16 8k;
+    gzip_http_version 1.1;
+    gzip_types text/plain text/css application/json application/javascript application/x-javascript text/xml application/xml application/xml+rss application/rss+xml text/javascript image/svg+xml application/vnd.ms-fontobject application/x-font-ttf font/opentype image/bmp image/png image/gif image/jpeg image/jpg;
 
-	# add ssl specific settings
-	# keepalive_timeout    240;
-	access_log  /var/log/nginx/oddo.access.log;
-	error_log   /var/log/nginx/oddo.error.log;    
-   
-	ssl                          on;
-	ssl_certificate              /etc/nginx/ssl/server.crt; 
-	ssl_certificate_key          /etc/nginx/ssl/server.key;
-	ssl_session_timeout          10h; 
-	ssl_protocols                SSLv3 TLSv1;
-	ssl_ciphers                  HIGH:!ADH:!MD5; 
-	ssl_prefer_server_ciphers    on;
-	keepalive_timeout   240; 
-
-	location / {
-	proxy_pass http://$NGINX_URL:8069/;
-	 }
+location / {
+  proxy_pass http://$DOMAIN_NAME:8069/;
+  }
 }
 EOF
 
@@ -301,6 +340,8 @@ sudo chown root:root $NGINX_CONFIG_AVAILABLE
 sudo chmod 640 $NGINX_CONFIG_AVAILABLE
 
 
+echo -e "\n---- Disabled the default site by deleting the symbolic link for it.  ----"
+#rm /etc/nginx/sites-available/default
 
 #activate site
 ln -s /$NGINX_CONFIG_AVAILABLE $NGINX_CONFIG_ENABLED
@@ -315,18 +356,18 @@ echo ""
 echo "	The server is available internaly:"
 echo "	http://localhost:8069 "
 echo "	The server is available externaly:"
-echo "	$NGINX_URL_PREFIX$NGINX_URL:$NGINX_PORT"
+echo "	$DOMAIN_NAME_PREFIX$DOMAIN_NAME:$NGINX_PORT"
 echo "******************************************************************"
 
 
 echo "-----------------------------------------------------------"
 echo "Done! The Odoo server is up and running. Specifications:"
-echo "Port: $OE_PORT"
-echo "User service: $OE_USER"
-echo "User PostgreSQL: $OE_USER"
-echo "Code location: $OE_USER"
-echo "Addons folder: $OE_USER/$OE_CONFIG/addons/"
-echo "Start Odoo service: sudo service $OE_CONFIG start"
-echo "Stop Odoo service: sudo service $OE_CONFIG stop"
-echo "Restart Odoo service: sudo service $OE_CONFIG restart"
+echo "Port: $ODOO_PORT"
+echo "User service: $ODOO_USER"
+echo "User PostgreSQL: $ODOO_USER"
+echo "Code location: $ODOO_USER"
+echo "Addons folder: $ODOO_USER/$ODOO_CONFIG/addons/"
+echo "Start Odoo service: sudo service $ODOO_CONFIG start"
+echo "Stop Odoo service: sudo service $ODOO_CONFIG stop"
+echo "Restart Odoo service: sudo service $ODOO_CONFIG restart"
 echo "-----------------------------------------------------------"
