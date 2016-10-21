d7fb1816dff33d2487e7e2ace0c5ac3fdbb182f3
Abdulhamid Ibrahim Alsalman <abdulhamid.alsalman@gmail.com>
2016-10-22 00:57:36 +0300

    Update odoo_nginx_install.sh

--- odoo_nginx_install.sh
+++ odoo_nginx_install.sh
@@ -83,6 +83,8 @@ sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $ODOO_USER" 2> /dev/null || true
 
+#TODO createuser --createdb --username postgres --no-createrole --no-superuser --pwprompt odoo
+
 sudo service postgresql restart
 
 #--------------------------------------------------
